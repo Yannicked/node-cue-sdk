688626655d80d36d8434352b2816ad48fbb74eb2
Yannick de Jong <yannickdejong@me.com>
2015-12-12 13:45:16 +0100

    Improved SDK downloading

--- getdll.js
+++ getdll.js
@@ -4,10 +4,28 @@ var unzip = require('unzip')
 var rimraf = require('rimraf');
 
 var uri = 'http://softwaredownloads.corsair.com/Files/Gaming-Keyboards/Corsair-SDK-Release-v1.10.73.zip';
-fs.mkdirSync(__dirname+'/tmp/')
-var s = request(uri).pipe(unzip.Extract({ path: __dirname+'/tmp/' }));
-s.on('finish', function () {
-	fs.renameSync(__dirname+'/tmp/CUESDK/bin/', __dirname+'/bin/')
-	fs.renameSync(__dirname+'/bin/x64/CUESDK.x64_2013.dll', __dirname+'/bin/x64/CUESDK_2013.dll')
-	rimraf(__dirname+'/tmp/', function(){});
+
+function main () {
+	fs.mkdirSync(__dirname+'/tmp/')
+
+	console.log('Downloading and Extracting CUESDK');
+	
+	var s = request(uri)
+	s.on('response', function (response) {
+		s.pipe(unzip.Extract({ path: __dirname+'/tmp/' }));
+		s.on('end', function () {
+			console.log('Cleaning up CUESDK');
+			fs.renameSync(__dirname+'/tmp/CUESDK/bin/', __dirname+'/bin/')
+			fs.renameSync(__dirname+'/bin/x64/CUESDK.x64_2013.dll', __dirname+'/bin/x64/CUESDK_2013.dll')
+			rimraf(__dirname+'/tmp/',  function(){});
+		});
+	});
+	s.on('error', function(e) {
+		console.log(e);
+		throw new Error('Error while downloading CUESDK')
+	})
+}
+
+rimraf(__dirname+'/tmp/', function() {
+	rimraf(__dirname+'/bin/', main)
 });
