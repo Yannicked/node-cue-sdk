5a0e08c48e019aa448e7f6c779d9d6f8edde5edc
Yannick de Jong <yannickdejong@me.com>
2016-08-02 22:39:31 +0200

    Added documentation

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-//node --harmony_destructuring --harmony_default_parameters
-
 var ffi = require('ffi');
 var ref = require('ref');
 var StructType = require('ref-struct');
@@ -11,10 +9,43 @@ var utils = require(__dirname+'/lib/utils.js')
 var path = require('path');
 var ArrayType = require('ref-array');
 
+/**
+	* The id of a led on the keyboard
+	* @typedef CorsairLedId
+	* @type {number}
+*/
 var CorsairLedId = ref.types.int;
+/**
+	* The access mode to be reported to the SDK.
+	* @typedef CorsairAccessMode
+	* @type {number}
+*/
 var CorsairAccessMode = ref.types.int;
+/**
+	* A error code as reported by the SDK.
+	* @typedef CorsairError
+	* @type {number}
+*/
 var CorsairError = ref.types.int;
 
+/**
+	* A rgb color
+	* @typedef color
+	* @type {Array}
+	* @property {number} 0 - Red intensity value 0..255
+	* @property {number} 1 - Green intensity value 0..255
+	* @property {number} 2 - Blue intensity value 0..255
+*/
+
+/**
+	* A rgb color with CorsairLedId included
+	* @typedef CorsairLedColor
+	* @type {Array}
+	* @property {number|string} 0 - Key id or name.
+	* @property {number} 1 - Red intensity value 0..255
+	* @property {number} 2 - Green intensity value 0..255
+	* @property {number} 3 - Blue intensity value 0..255
+*/
 var CorsairLedColor = StructType({
 	ledId: CorsairLedId,
 	r: ref.types.int,
@@ -35,6 +66,15 @@ var CorsairDeviceInfo = StructType({
 
 var CorsairDeviceInfoPtr = ref.refType(CorsairDeviceInfo);
 
+/**
+	* @typedef CorsairLedPosition
+	* @property {CorsairLedId} ledId - The id of the led.
+	* @property {number} top - The key's distance from the top of the keyboard.
+	* @property {number} left - The key's distance from the left of the keyboard.
+	* @property {number} height - The key's height.
+	* @property {number} width - The key's width.
+*/
+
 var CorsairLedPosition = StructType({
 	ledId: CorsairLedId,
 	top: ref.types.double,
@@ -52,6 +92,16 @@ var CorsairLedPositions = StructType({
 
 var CorsairLedPositionsPtr = ref.refType(CorsairLedPositions);
 
+/**
+	* The protocol details as reported by the SDK.
+	* @typedef CorsairProtocolDetails
+	* @type {Object}
+	* @property {string} sdkVersion - The version number of the SDK.
+	* @property {string} serverVersion - The version number of the server.
+	* @property {number} sdkProtocolVersion - The protocol version number.
+	* @property {number} serverProtocolVersion - The server's protocol version number.
+	* @property {boolean} breakingChanges - True if this update contained breaking changes.
+*/
 var CorsairProtocolDetails = StructType({
 	sdkVersion: ref.types.CString,
 	serverVersion: ref.types.CString,
@@ -60,6 +110,10 @@ var CorsairProtocolDetails = StructType({
 	breakingChanges: ref.types.bool
 });
 
+/**
+	* A error with the CUE sdk.
+	* @private
+*/
 function CueError(err) {
     this.name = "CueError";
     this.message = enums.CorsairError.get(Math.pow(2, err)).key+((err>3&&err!=5)?' -- this might be an error in the CueSDK wrapper, please contact the developer.':'');
@@ -70,8 +124,20 @@ function CueError(err) {
 
 CueError.prototype = Error.prototype;
 
+/**
+	* The main CueSDK class.
+	@property {CorsairProtocolDetails} this.details - The protocol details as reported by the SDK.
+	@property {CorsairError} this.lastError - The last error as reported by the SDK.
+	@property {number} this.fps - The frames-per-second for fading.
+	@property {string} this.fadeType - The type of fading to be used.
+*/
 class CueSDK {
-	constructor(clear = false, exclusive = false) { // libLocation = Full path of DLL file, if clear is true, the current lights will be cleared
+	/**
+		* Create a Cue SDK object and do a handshake with the sdk.
+		* @param {boolean} [clear=false] - If true, clear the all the leds.
+		* @param {boolean} [exclusive=false] - If true, enable exclusive mode.
+	*/
+	constructor(clear = false, exclusive = false) {
 		this.CueSDKLib = ffi.Library(path.join(__dirname, 'bin', process.arch, 'CUESDK_2013.dll'), {
 			'CorsairSetLedsColors': ['bool', ['int', 'pointer']],
 			'CorsairSetLedsColorsAsync': ['bool', ['int', 'pointer', 'pointer', 'pointer']],
@@ -83,23 +149,37 @@ class CueSDK {
 			'CorsairPerformProtocolHandshake': [CorsairProtocolDetails, []],
 			'CorsairGetLastError': [CorsairError, []]
 		});
+
 		this.details = this.CueSDKLib.CorsairPerformProtocolHandshake().toObject();
 		this.lastError = 0;
 		this._error();
-		
+
 		// Request exclusive access to keyboard LEDs
 		if(exclusive) this.CueSDKLib.CorsairRequestControl(enums.CorsairAccessMode.CAM_ExclusiveLightingControl);
-		
+
 		this.fps = 30;
 		this.fade_helper = new utils.fade();
 		this.fadeType = 'Wheel';
-		
+
 		if (clear) {
 			this.clear();
 		}
-		
+
 		return this;
 	}
+
+	/**
+		* This callback runs after the leds have been set asynchronously.
+		*
+		* @callback setCallback
+	*/
+
+	/**
+		* Set the led colors.
+		* @param {(CorsairLedColor[]|...number|...string)} led - The key id with rgb values as an array.
+		* @param {setCallback} [callback] - The callback ran after completing the asynchonous request.
+		* @param {boolean} [ids=false] - If true, use the key ids as-is.
+	*/
 	set() {
 		if (arguments[0] instanceof Array) {
 			if (typeof(arguments[1]) === 'function') {
@@ -115,7 +195,12 @@ class CueSDK {
 			}
 		}
 	}
-	setSync(a, ids = false) { // a = array of leds [key, r, g, b] (r, g, b should be [0..255])
+	/**
+		* Set multiple led colors synchronously.
+		* @param {CorsairLedColor[]} a - The key ids or names with rgb values as an array.
+		* @param {boolean} [ids=false] - If true, use the key ids as-is.
+	*/
+	setSync(a, ids = false) {
 		let l = [];
 		if (ids) {
 			for (let i = 0; i<a.length; i++) {
@@ -135,6 +220,14 @@ class CueSDK {
 			return this;
 		}
 	}
+	/**
+		* Set an individual led colors synchronously.
+		* @param {(number|string)} key - The key id or name.
+		* @param {number} r - Red intensity value 0..255
+		* @param {number} g - Green intensity value 0..255
+		* @param {number} b - Blue intensity value 0..255
+		* @param {boolean} [ids=false] - If true, use the key ids as-is.
+	*/
 	setIndividualSync(key, r, g, b, ids = false) {
 		let l = this._getLedColor(ids?key:this._getLedIdForKeyName(key), r, g, b).ref();
 		let re = this.CueSDKLib.CorsairSetLedsColors(1, l);
@@ -145,7 +238,13 @@ class CueSDK {
 			return this;
 		}
 	}
-	setAsync(a, callback, ids = false) { // a = array of leds [key, r, g, b] (r, g, b should be [0..255])
+	/**
+		* Set multiple led colors synchronously.
+		* @param {CorsairLedColor[]} a - The key id with rgb values as an array.
+		* @param {setCallback} callback - The callback ran after completing the asynchonous request.
+		* @param {boolean} [ids=false] - If true, use the key ids as-is.
+	*/
+	setAsync(a, callback, ids = false) {
 		let l = [];
 		if (ids) {
 			for (let i = 0; i<a.length; i++) {
@@ -172,6 +271,15 @@ class CueSDK {
 			return asyncFunc;
 		}
 	}
+	/**
+		* Set an individual led colors synchronously.
+		* @param {(number|string)} key - The key id or name.
+		* @param {number} r - Red intensity value 0..255
+		* @param {number} g - Green intensity value 0..255
+		* @param {number} b - Blue intensity value 0..255
+		* @param {setCallback} callback - The callback ran after completing the asynchonous request.
+		* @param {boolean} [ids=false] - If true, use the key ids as-is.
+	*/
 	setIndividualAsync(key, r, g, b, callback, ids = false) {
 		let l = this._getLedColor(ids?key:this._getLedIdForKeyName(key), r, g, b).ref();
 		let asyncFunc = ffi.Callback('void', ['pointer', 'bool', CorsairError], (context, succes, error) => {
@@ -189,6 +297,21 @@ class CueSDK {
 			return asyncFunc;
 		}
 	}
+	/**
+		* This callback runs after the leds have been faded asynchronously.
+		*
+		* @callback fadeCallback
+	*/
+
+	/**
+		* Fade multiple or individual leds.
+		* @param {(number|number[])} k - Key or arrray of keys to be faded.
+		* @param {color} f - The starting color.
+		* @param {color} t - The color to fade to.
+		* @param {number} l - Time to fade in miliseconds
+		* @param {fadeCallback} cb - The callback ran after completing the asynchonous request.
+		* @param {boolean} [ids=false] - If true, use the key ids as-is.
+	*/
 	fade() {
 		if (arguments[0] instanceof Array) {
 			return this.fadeAsync(...arguments);
@@ -196,7 +319,16 @@ class CueSDK {
 			return this.fadeIndividualAsync(...arguments);
 		}
 	}
-	fadeAsync(k, f, t, l, cb = () => {}, ids = false) { // k = array of leds, f = from color, t = to color [r, g, b], l = time in ms
+	/**
+		* Fade multiple or individual leds.
+		* @param {number[]} k - Key or arrray of keys to be faded.
+		* @param {color} f - The starting color.
+		* @param {color} t - The color to fade to.
+		* @param {number} l - Time to fade in miliseconds
+		* @param {fadeCallback} cb - The callback ran after completing the asynchonous request.
+		* @param {boolean} [ids=false] - If true, use the key ids as-is.
+	*/
+	fadeAsync(k, f, t, l, cb = () => {}, ids = false) {
 		this.fade_helper[this.fadeType](f, t, l, this.fps, (r, g, b) => {
 			let a = [];
 			for (let i = 0; i<k.length; i++) {
@@ -205,11 +337,23 @@ class CueSDK {
 			this.setAsync(a, cb, ids);
 		});
 	}
+	/**
+		* Fade multiple or individual leds.
+		* @param {number} k - Key or arrray of keys to be faded.
+		* @param {color} f - The starting color.
+		* @param {color} t - The color to fade to.
+		* @param {number} l - Time to fade in miliseconds
+		* @param {fadeCallback} cb - The callback ran after completing the asynchonous request.
+		* @param {boolean} [ids=false] - If true, use the key ids as-is.
+	*/
 	fadeIndividualAsync(k, f, t, l, cb = () => {}, ids = false) { // k = array of leds, f = from color, t = to color [r, g, b], l = time in ms
 		this.fade_helper[this.fadeType](f, t, l, this.fps, (r, g, b) => {
 			this.setIndividualAsync(k, r, g, b, (r == t[0] && g == t[1] && b == t[2])?cb:(() => {}), ids);
 		});
 	}
+	/**
+		* Set all leds to black.
+	*/
 	clear() {
 		let l = [];
 		for (let i = 1; i<=154; i++) {
@@ -217,23 +361,42 @@ class CueSDK {
 		}
 		this.set(l, true);
 	}
+	/**
+		* Get the leds as reported by the CueSDK
+		* @return {CorsairLedPosition[]} l - An array containing the led's positions as reported by the sdk.
+	*/
 	getLeds() {
 		let p = this.CueSDKLib.CorsairGetLedPositions().deref();
 		let l = p['pLedPosition'];
 		l.length = p['numberOfLeds'];
 		return l;
 	}
+	/**
+		* Close the connection with the SDK and release all keys.
+	*/
 	close() {
 		this.CueSDKLib._dl.close();
 		this.CueSDKLib = {};
 	}
+	/**
+		* Create a CorsairLedColor.
+		* @private
+	*/
 	_getLedColor(ledId, r, g, b) {
 		let keyColor = new CorsairLedColor({ledId, r, g, b});
 		return keyColor;
 	}
+	/**
+		* Try to get a ledId for a key's name.
+		* @private
+	*/
 	_getLedIdForKeyName(key) {
 		return enums.CorsairLedId.get('CLK_'+key).value;
 	}
+	/**
+		* Check if the SDK returned a error.
+		* @private
+	*/
 	_error() {
 		this.lastError = this.CueSDKLib.CorsairGetLastError();
 		if (this.lastError != 'CE_Success' && this.lastError != 0) {
